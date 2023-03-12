5d6963a7544c061b47d79fcf29f63c0fa0079481
Tosasitill <66861785+tosasitill@users.noreply.github.com>
2023-03-12 16:17:16 +0800

    Update lineage_polaris.mk

--- lineage_polaris.mk
+++ lineage_polaris.mk
@@ -26,54 +26,21 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-RICE_CHIPSET := "snapdragon 845"
+RICE_CHIPSET := sdm845
 
-# chipset flag enclose var with "" if more than one
-# friendly tip: builders can use init_xxx.cpp as workaround for spacing
-# e.g. property_override("ro.rice.maintainer", "get riced");
-RICE_MAINTAINER := "tosasitill"
+RICE_MAINTAINER := "tosasitill
 
-# chipset flag enclose var with "" if more than one
-# this will reflect on build/display version, a firmware package/zip name 
-# e.g. riceDroid-7.0-COMMUNITY-device-AOSP.zip - AOSP is the default package type, WITH_GMS will override the package type to PIXEL
-RICE_PACKAGE_TYPE := "VANILLA AOSP"
-
-# Sushi Bootanimation (only 720/1080p/1440 supported. if not defined, bootanimation is google bootanimation)
 SUSHI_BOOTANIMATION := 1080
 
-# Aperture Camera (default: not defined - skipped by the compiler)
-TARGET_BUILD_APERTURE_CAMERA := false
+#adapt for polaris 
+# WITH GMS
 
-# Graphene Camera (default: not defined - skipped by the compiler)
-TARGET_BUILD_GRAPHENEOS_CAMERA := false
-
-# disable/enable blur support, default is false
+WITH_GMS := true
+SUSHI_BOOTANIMATION := 1080
 TARGET_ENABLE_BLUR := true
-
-# UDFPS ICONS/ANIMATIONS
-TARGET_HAS_UDFPS := true
-
-# Allow usage of custom binary linker (LD), default is false
-TARGET_KERNEL_OPTIONAL_LD := false
-
-# Spoof build description/fingerprint as pixel device
-TARGET_USE_PIXEL_FINGERPRINT := false
-
-WITH_GMS := false 
-
-#- ship with GMS packages, replaces misc AOSP packages with Google packages.
-
-# Customized GMS Flags 
-# WITH_GMS flag is required
-
-# Wether to use google (true) or AOSP (false) telephony package bundle. (defaults: false for gms core, true for pixel builds)
+TARGET_USE_PIXEL_FINGERPRINT := true
 TARGET_USE_GOOGLE_TELEPHONY := false
-
-# Compiler will only build GMS playstore services, its dependencies, and Gboard app.
-# package type will change from PIXEL/GMS -> CORE
-TARGET_CORE_GMS := false
+TARGET_SUPPORTS_QUICK_TAP := true
 
 ro.apk_verity.mode=2
 
-# extra flag under TARGET_CORE_GMS
-TARGET_CORE_GMS_EXTRAS := true/false - extra packages for core build type (velvet and photos)
